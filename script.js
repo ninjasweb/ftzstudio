540b254fa65ce5efe2ee45b028e582db8e9c7d3a
NinjasWeb <hola@ninjasweb.com>
2020-02-06 00:43:39 -0500

    Agregado efecto Drag and Drop
    Agregadas las carpetas y la papelera

---
+++ script.js
@@ -0,0 +1,14 @@
+$(function(){
+     $("#draggable").draggable();
+});
+
+$(function(){
+     $("#draggable2").draggable();
+});
+
+$(function(){
+     $("#draggable3").draggable();
+});
+
+document.getElementById("modal").style.display = "none";
+
